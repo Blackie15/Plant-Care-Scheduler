8deab0e8451a36238922fa23cca8db4db1fbd69e
Suchitra Km <suchitrakm2020@gmail.com>
2026-06-25 11:51:12 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/suchitra/plantcarescheduler/PlantcareschedulerApplication.java
@@ -0,0 +1,13 @@
+package com.suchitra.plantcarescheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantcareschedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantcareschedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suchitra/plantcarescheduler/PlantcareschedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suchitra.plantcarescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantcareschedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
